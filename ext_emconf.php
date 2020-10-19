73ac86b50fb3299c93e1d13f9f02f1bc572a26ee
Stefan Froemken <froemken@gmail.com>
2020-10-19 11:10:32 +0200

    Remove unused clearCacheOnLoad

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,12 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Pforzheim Flexslider',
-    'description' => 'This extension provides a FlexSlider Plugin for Pforzheim',
+    'title' => 'PF Flexslider',
+    'description' => 'Pfflexslider provides a Slider based on JS Framework FlexSlider',
     'category' => 'plugin',
     'author' => 'Stefan Froemken',
     'author_email' => 'projects@jweiland.net',
     'state' => 'stable',
-    'clearCacheOnLoad' => 0,
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
